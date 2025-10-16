66d21ae3d08de87395d17cceb01236ea7f33baf3
Akarshana06 <akarshana027@gmail.com>
2025-10-16 12:29:32 +0530

    projects and love letters done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+// ...existing code...
 import React from 'react'
 import LandingPage from './pages/LandingPage.jsx'
-
+import Project from './pages/project-feedback.jsx'
+// ...existing code...
 export default function App() {
-  return <LandingPage />
+  return (
+    <>
+      <LandingPage />
+      <Project />
+    </>
+  )
 }
+// ...existing code...
